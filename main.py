93523a443bd9c8fbfb9ad5dbdf518dbad9306e67
Alicia <aliciap@magou.fr>
2026-02-13 10:59:05 +0100

    Petite réduction fini

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import pandas as pd
 from dateutil.relativedelta import relativedelta
 from fonction_de_base import *
 
+fileName = "data.xlsx"
+
 def readExcel(fileName, id):
     #Ont récupère les 4 dernier du client
     df = pd.read_excel(fileName)
     df = df[df["ID"] == id]
-    df = df.tail(4)
+    df = df[["ID", "Date", "Réduction"]]
 
     # Vérifie le format de date et les trie
     df["Date"] = pd.to_datetime(df["Date"])
@@ -14,22 +16,14 @@ def readExcel(fileName, id):
 
     return df
 
+def calcule(df):
+    for i in range(2, len(df)):
+        if (df["Réduction"].iloc[i] == "Aucune réduction" and
+        df["Réduction"].iloc[i-1] == "Aucune réduction" and
+        df["Réduction"].iloc[i-2] == "Aucune réduction"):
+            df.loc[df.index[i], "Réduction"] = "Réductions"
+    print(df)
 
-def calcul(df):
-    resultats = {}
-
-    for id_val, group in df.groupby("ID"):
-        # Si moin de 4 rendez vous, renvoi False et arrète
-        if len(df) < 4:
-            resultats[id_val] = False
-            continue
-
-        # Calcule si les 4 derniée rendez vous ont eu lieu dans une fentre de 3 mois ou moin
-        resultats[id_val] = df["Date"].max() <= df["Date"].min() + relativedelta(months=3)
-        print(resultats)
-
-    return df
-
-dataFram = readExcel("data.xlsx", 74698)
-x = calcul(dataFram)
-#print(dataFram)
+dataFram = readExcel("data.xlsx", "Théa")
+calcule(dataFram)
+# print(dataFram)
